4f574693758f9633d6975eadbf0851b79b52a0e8
P ch <pkchvyrov@vk.com>
2023-12-08 18:08:00 +0300

    init commit

---
+++ hangman-game/hangman-game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hangman-game
+//
+//  Created by Павел Чвыров on 08.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
